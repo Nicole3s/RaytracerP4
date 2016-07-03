775449cc811a9b102c39f51e7dfc844ae5ed8520
Nicole van den Dries <n.j.a.vandendries@students.uu.nl>
2016-07-03 19:08:34 +0200

    template met lege klassen

---
+++ game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Template {
+
+class Game
+{
+	// member variables
+	public Surface screen;
+    public Surface debug;
+    public Surface raytracer;
+	// initialize
+	public void Init()
+	{
+	}
+	// tick: renders one frame
+	public void Tick()
+	{
+		screen.Clear( 0 );
+		raytracer.Print( "raytracer", 2, 2, 0xff55ff );
+        debug.Print("debug", 2, 2, 0xff55ff);
+
+        raytracer.CopyTo(screen, 0, 0);
+        debug.CopyTo(screen,512, 0);
+	}
+}
+
+} // namespace Template
